8daf24345bfb0a72c67ce44732f1da74f053e35c
GirliOS <myladyios@gmail.com>
2015-08-04 18:02:02 +0800

    first commit

---
+++ WatchWeatherKit/WatchWeatherKit.h
@@ -0,0 +1,19 @@
+//
+//  WatchWeatherKit.h
+//  WatchWeatherKit
+//
+//  Created by girlios on 8/4/15.
+//  Copyright © 2015 GirliOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatchWeatherKit.
+FOUNDATION_EXPORT double WatchWeatherKitVersionNumber;
+
+//! Project version string for WatchWeatherKit.
+FOUNDATION_EXPORT const unsigned char WatchWeatherKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchWeatherKit/PublicHeader.h>
+
+
